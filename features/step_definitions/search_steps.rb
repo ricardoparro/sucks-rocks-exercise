14e1913e542eb2a9e37404497bd8317ba390fd56
Ricardo Parro <ricardo.parro@gmail.com>
2016-01-31 19:02:46 +0100

    add features and scenarios

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,9 @@
+When(/^I search for (.*)$/)  do |term|
+    @scores ||= {}
+    @scores[term] = RockScore.for_term(term)
+end
+
+
+Then(/^Apple should have a higher score than microsoft$/) do
+      pending # Write code here that turns the phrase above into concrete actions
+end
