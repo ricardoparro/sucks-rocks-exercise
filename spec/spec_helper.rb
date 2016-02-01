1ad7826c19db02766f0da5c8626f1e977450594d
Ricardo Parro <ricardo.parro@gmail.com>
2016-02-01 21:26:16 +0100

    config on rspec to allow shoulda and stubs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,9 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    config.mock_with :rspec do |c|
+        c.syntax = [:should, :expect]
+    end
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
